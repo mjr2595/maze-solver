29fab53eafdfefc36d0a42390bd0e66dc6360339
mjr2595 <michaeljohnraymond@gmail.com>
2025-04-26 00:27:11 -0500

    Add line drawing to the graphics module

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from graphics import Window
+from graphics import Line, Point, Window
 
 
 def main():
     win = Window(800, 600)
+    l = Line(Point(50, 50), Point(400, 400))
+    win.draw_line(l, "black")
     win.wait_for_close()
 
 
