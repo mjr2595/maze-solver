7969cbc811b61141e6391f9606be72a762c67025
mjr2595 <michaeljohnraymond@gmail.com>
2025-04-27 11:57:18 -0500

    Implement Maze class

---
+++ maze.py
@@ -0,0 +1,51 @@
+import time
+
+from cell import Cell
+
+
+class Maze:
+    def __init__(
+        self,
+        x1,
+        y1,
+        num_rows,
+        num_cols,
+        cell_size_x,
+        cell_size_y,
+        win,
+    ):
+        self._cells = []
+        self._x1 = x1
+        self._y1 = y1
+        self._num_rows = num_rows
+        self._num_cols = num_cols
+        self._cell_size_x = cell_size_x
+        self._cell_size_y = cell_size_y
+        self._win = win
+
+        self._create_cells()
+
+    def _create_cells(self):
+        self._cells = [
+            [Cell(self._win) for row in range(self._num_rows)]
+            for col in range(self._num_cols)
+        ]
+        for col in range(self._num_cols):
+            for row in range(self._num_rows):
+                self._draw_cell(col, row)
+
+    def _draw_cell(self, col, row):
+        if self._win is None:
+            return
+        x1 = self._x1 + col * self._cell_size_x
+        y1 = self._y1 + row * self._cell_size_y
+        x2 = x1 + self._cell_size_x
+        y2 = y1 + self._cell_size_y
+        self._cells[col][row].draw(x1, y1, x2, y2)
+        self._animate()
+
+    def _animate(self):
+        if self._win is None:
+            return
+        self._win.redraw()
+        time.sleep(0.02)
